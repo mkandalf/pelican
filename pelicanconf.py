473f46fbc0e2a4ba386605ecbb5f01d7c100ce85
Nick Meyer <nicholasmeyer@gmail.com>
2012-09-19 21:58:26 -0400

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Nick Meyer"
+SITENAME = u"Nick Meyer"
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+WITH_PAGINATION = False
+
+THEME = "ctheme"
