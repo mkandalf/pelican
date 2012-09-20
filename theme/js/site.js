552dc2d792f49b06b02694118234a8ca8476fefa
Nick Meyer <nicholasmeyer@gmail.com>
2012-09-20 07:01:44 +0500

    Update documentation

---
+++ theme/js/site.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    sidebar = $('#sidebar');
+    fading = false;
+    epsilon = 0.00001
+    function check_sidebar(e){
+        if (!fading){
+            dist = $(document).scrollTop();
+            opacity = sidebar.css('opacity')
+            if (dist > 200 && Math.abs(opacity-1) < epsilon){
+                fading = true;
+                $('#sidebar').fadeTo(600,0.2,function(){
+                    fading = false;
+                    $(document).trigger('scroll');
+                });
+            }
+            else if (dist < 200 && Math.abs(opacity-0.2) < epsilon){
+                fading = true;
+                $('#sidebar').fadeTo(600,1.0,function(){
+                    fading = false;
+                    $(document).trigger('scroll');
+                });
+            }
+        }
+    }
+    $(document).on('scroll',check_sidebar);
+    $('#sidebar').hover(function(e){
+        sidebar.css('opacity', 1);
+    }, check_sidebar);
+});
